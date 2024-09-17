e56c0edee1be7ba33d9693f1dd89262120c8fc5d
Maximiliano Salvatti <msalvatti@gmail.com>
2024-09-17 18:49:19 -0300

    Feature: Logo and favicon added

--- frontend-new/src/components/navbar/index.tsx
+++ frontend-new/src/components/navbar/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
 
-//import { Image } from '@chakra-ui/react'
-import { Box, Flex, useDisclosure } from '@chakra-ui/react'
+import { Image } from '@chakra-ui/react'
+import { Box, Flex, useDisclosure, useColorMode } from '@chakra-ui/react'
 
 import NavbarInfo from './info'
 import NavbarLanguages from './language'
-//import NavbarSettings from './settings'
 import NavbarTheme from './theme'
 import NavbarWallet from './wallet'
+import LogoDark from '../../assets/img/logo/dailyBids_black.svg'
+import LogoLight from '../../assets/img/logo/dailyBids_white.svg'
 import AccountComponent from '../account'
 
 const NavbarComponent: React.FC = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
+  const { colorMode } = useColorMode()
 
   return (
     <Flex
@@ -22,12 +24,18 @@ const NavbarComponent: React.FC = () => {
       borderBottom="1px solid"
       borderColor="gray.800"
     >
-      <Box>{/* <Image src={Logo} alt="Logo" /> */}</Box>
+      <Box>
+        <Image
+          src={colorMode === 'dark' ? LogoLight : LogoDark}
+          height="37px"
+          mb="3px"
+          alt="Logo"
+        />
+      </Box>
       <Flex flexDirection="row" alignItems="center">
         <NavbarLanguages />
         <NavbarTheme />
         <NavbarInfo />
-        {/*<NavbarSettings />*/}
         <NavbarWallet onOpen={onOpen} />
         <AccountComponent isOpen={isOpen} onClose={onClose} />
       </Flex>
